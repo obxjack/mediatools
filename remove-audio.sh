ae2aa24fc883baa4c7b789969480c9a339f303a7
obxjack <jack.barker@gmail.com>
2020-08-15 17:04:34 -0400

    ...

--- remove-audio.sh
+++ remove-audio.sh
@@ -10,8 +10,6 @@
 	converteddir=$(cat `dirname ${0}`/config.json | jq -r '.converteddir')
 	tmpjson=$(cat `dirname ${0}`/config.json | jq -r '.tmpjson')
 
-	echo "=========================================================================================="
-
 	OIFS="$IFS" ## needed for spaces in filenames 
 	IFS=$'\n'
 
@@ -19,7 +17,8 @@
 	((processed=0))
 
 	# Let's loop through the current directory and process all files
-	for x in $(ls -1 ${wrkdir}); do # | sed 's/\.[a-z]*//g'); do
+#	for x in $(ls -1 ${wrkdir}); do
+	for x in $(find $wrkdir -type f | grep -v ".DS_Store"); do
 		wrkfname=$x
 
 		# Use ffprobe to pull details of the streams in the video file
@@ -30,7 +29,8 @@
 		((streamcount=0))
 		((audiostreams=0))
 		audioidx=()
-		
+		filesize=$(stat -f%z "$wrkfname")
+
 		until [ $codec_type == "null" ]; do
 			codec_type=$(cat $tmpjson | jq -r --argjson count "$streamcount" '.streams | .[$count] | .codec_type')
 			
@@ -44,7 +44,7 @@
 				language=$(cat $tmpjson | jq -r --argjson count "$streamcount" '.streams | .[$count] | .tags.language')
 
 				if [[ ${language} == "eng" ]]; then
-					# I found an audio stream -- adding to audioidx array
+					# I found an english audio stream -- adding to audioidx array
 
 					audioidx+=($streamcount)
 				fi
@@ -55,25 +55,45 @@
 				
 		if [[ $audiostreams -gt 1 ]]; then
 			# I found a video with more than one audio stream -- time to remove non-english audio
+						
+			english_streams=${#audioidx[@]}
 
-			filesize=$(stat -f%z "$wrkfname")
-			((processed++))
+			audio_map=""
+			if [[ ${english_streams} -gt 1 && ${english_streams} != ${audiostreams} ]]; then
 
-			echo "Input  File     : ${wrkfname}"
-			echo "Output File     : ${converteddir}/${wrkfname}"
-			echo "Source Filesize : ${filesize}"
-			echo "Video Index     : ${videoidx}"
-			echo "Audio Index     : ${audioidx[@]}"
-			
-			#add loop here to build audio map if more than 1 English stream found
-			echo "Audio Index     : ${audioidx[0]}"
-			ffmpeg -hide_banner -loglevel quiet -i $wrkfname -map 0:${videoidx} -map 0:${audioidx[0]} -vcodec copy -acodec copy "${converteddir}/${wrkfname}"
-			
-			filesize=$(stat -f%z "${converteddir}/${wrkfname}")
-			echo "Output Filesize : ${filesize}"
-			echo "==============="
+				for x in ${!audioidx[@]}; do
+					audio_map="${audio_map} -map 0:${audioidx[x]} "
+				done
+				outfname="${converteddir}/`basename ${wrkfname}`"
+
+				echo "==================================================================================================================="
+				echo "Input  File     : ${wrkfname}"
+				echo "Source Filesize : ${filesize}"
+				echo "Output File     : ${outfname}"
+				echo "Audio Map       : ${audio_map}"
+
+				cmdline="ffmpeg -hide_banner -loglevel quiet -i \"${wrkfname}\" -map 0:${videoidx}${audio_map} -vcodec copy -acodec copy \"${outfname}\""
+				
+				eval $cmdline
+				echo ""
+				
+				#ffmpeg $cmdline
+				#ffmpeg -hide_banner -loglevel quiet -i $wrkfname -map 0:v:${videoidx} ${audio_map} -vcodec copy -acodec copy "${converteddir}/`basename ${wrkfname}`"
+				#ffmpeg $wrkfname -map 0:${videoidx} ${audio_map} -vcodec copy -acodec copy "${outfname}"
+				((processed++))
+
+				filesize=$(stat -f%z "${converteddir}/`basename ${wrkfname}`")
+				echo "Output Filesize : ${filesize}"
+				echo ""
+
+			else
+				echo "==================================================================================================================="
+				echo "Input  File     : ${wrkfname}"
+				echo "The media file contains more than one audio streams (${audiostreams}) however all of them are English... skipping."
+			fi 			
 		fi
 		datevar=$(date +'%d-%b-%Y %H:%M:%S')
+		#echo ""
 	done
 	echo "Videos Processed: ${processed}"
 
